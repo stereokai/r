d008d0725533efc0cd4912bc496e72bb8438c73b
Stereokai <Email me at tom@ (my website url)>
2015-09-20 07:57:24 +0300

    Add uncss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var uncss = require('gulp-uncss');
 
 gulp.task('default', function () {
     return gulp.src('css/index.css')
+        .pipe(uncss({
+            html: ['index.html']
+        }))
         .pipe(autoprefixer({
             browsers: ['last 3 versions', '> 5%'],
             cascade: false
